6fc4a187b32b8de70e2d62f72ea8d2cb59083ce6
Joachim Gabler <jgabler@hpc-gridware.com>
2024-07-01 16:17:38 +0200

    TA: CS-371 move Java code to a separate repository and build it as Maven project // initial checkin

---
+++ Readme.md
@@ -0,0 +1,55 @@
+# Cluster Scheduler DRMAA Java API
+
+The DRMAA Java is a wrapper for around the [Cluster Scheduler](https://github.com/hpc-gridware/clusterscheduler)
+DRMAA version 1 C/C++ API.
+It uses JNI to access the libdrmaa shared library.
+
+It comes with howtos (same logic as the DRMAA C howtos but in Java) and Javadoc documentation.
+A comprehensive testsuite can be run from within the Cluster Scheduler testsuite.
+
+## Prerequisits
+
+Cluster Scheduler DRMAA Java requires the following Software for Building
+* Java JDK >= 1.8
+* Maven
+* A Cluster Scheduler installation including the architecture specific libdrmaa.so
+
+## Build / Package
+
+Build the jdrmaa jar file with Maven and copy it to the Cluster Scheduler installation:
+```Shell
+mvn package
+cp target jdrmaa-1.0.jar $SGE_ROOT/lib
+```
+
+## Basic Test
+
+A basic DRMAA Java example application `DrmaaExample` (`src/main/java/DrmaaExample.java`) is part of the DRMAA Java jar file.
+In order to run it you need a Cluster Scheduler installation, the host running the example
+must be submit host.
+
+The example application submits a number of array jobs and sequential jobs and waits
+for them get scheduled and to finish.
+
+```Shell
+source $SGE_ROOT/$SGE_CELL/common/settings.sh
+java -cp target/jdrmaa-1.0.jar DrmaaExample $SGE_ROOT/examples/jobs/sleeper.sh
+```
+
+## Running the Test Suite
+
+Testing the Cluster Scheduler DRMAA Java API requires and is called from
+the Cluster Scheduler testsuite.
+The testsuite will be open sourced in the near future.
+
+## License
+
+The original files contributed by Sun Microsystems are licensed under
+
+- [Sun Industry Standards Source License v1.2](https://github.com/hpc-gridware/drmaa-java/blob/master/License_SISSL_v1-2.txt)
+
+Newer contributions from HPC-Gridware GmbH (and others like Univa Inc. or individual contributors) are licensed under
+
+- [Apache License v2.0](https://github.com/hpc-gridware/drmaa-java/blob/master/License_APACHE_v2-0.txt)
+
+
---
+++ src/test/java/com/sun/grid/Settings.java
@@ -0,0 +1,96 @@
+/*___INFO__MARK_BEGIN__*/
+/*************************************************************************
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  the Sun Industry Standards Source License Version 1.2
+ *
+ *  Sun Microsystems Inc., March, 2001
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.2
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.2 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://gridengine.sunsource.net/Gridengine_SISSL_license.html
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *   The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *   Copyright: 2001 by Sun Microsystems, Inc.
+ *
+ *   All Rights Reserved.
+ *
+ ************************************************************************/
+/*___INFO__MARK_END__*/
+
+/*
+ * Settings.java
+ *
+ * Created on September 7, 2005, 4:01 PM
+ */
+
+package com.sun.grid;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *
+ * @author  dan.templeton@sun.com
+ */
+public class Settings {
+    private static Map settings = new HashMap();
+    public static final String USER = "user";
+    public static final String TEST_PATH = "test_path";
+    public static final String SGE_ROOT = "sge_root";
+    public static final String ARCH = "arch";
+    public static final String HOSTNAME = "hostname";
+    public static final String GETS_ROOT = "gets_root";
+    public static final String CWD = "cwd";
+    public static final String SCRIPTS_DIR = "scripts_dir";
+    public static final String CHECKTREE_ROOT = "checktree_root";
+    public static final String PID = "pid";
+    public static final String HOME_DIR = "home_dir";
+    public static final String VERSION = "version";
+    
+   /* The first time this class is used, it will initialize all of the values
+    * automatically. */
+    static {
+        try {
+            settings.put(USER, System.getProperty("gets.user", "/"));
+            settings.put(TEST_PATH, System.getProperty("gets.test_path", "/"));
+            settings.put(SGE_ROOT, System.getProperty("gets.sge_root", "/"));
+            settings.put(ARCH, System.getProperty("gets.arch", ""));
+            settings.put(HOSTNAME, System.getProperty("gets.hostname", "unknown"));
+            settings.put(GETS_ROOT, System.getProperty("gets.gets_root", "/"));
+            settings.put(CWD, System.getProperty("gets.cwd", "/"));
+            settings.put(SCRIPTS_DIR, System.getProperty("gets.scripts_dir", "/"));
+            settings.put(CHECKTREE_ROOT, System.getProperty("gets.checktree_root", "/"));
+            settings.put(PID, System.getProperty("gets.pid", "-1"));
+            settings.put(HOME_DIR, System.getProperty("gets.home_dir", "/"));
+            settings.put(VERSION, System.getProperty("gets.version", "maintrunk"));
+        } catch (SecurityException e) {
+            System.err.println("Not allowed to access system properties");
+            System.exit(1);
+        }
+    }
+    
+    public static String get(String setting) {
+        return (String)settings.get(setting);
+    }
+    
+    public static void put(String setting, String value) {
+        settings.put(setting, value);
+    }
+    
+    public String toString() {
+        return settings.toString();
+    }
+}
